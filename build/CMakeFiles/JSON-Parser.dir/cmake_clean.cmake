c4a31b6beb2e197c109302cdcaca37d5f6677a24
totoo7 <stoyanorozov@gmail.com>
2024-03-29 19:00:09 +0200

    Init commit

---
+++ build/CMakeFiles/JSON-Parser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JSON-Parser.dir/main.cpp.o"
+  "CMakeFiles/JSON-Parser.dir/main.cpp.o.d"
+  "JSON-Parser"
+  "JSON-Parser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JSON-Parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
